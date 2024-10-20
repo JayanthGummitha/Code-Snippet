a5972200fa1d6ca8aed8dbbf53a716f9f983e088
JayanthGummitha <jayanthgummitha@gmail.com>
2024-10-20 16:01:42 -0500

    code-snippet-frontend

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"DEWB53cLFvZEuxlWFX/64fd5fbvDqK7mDs25BkXkpqI=\"}"
--- code-snippet-frontend/components/Editor/Views.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/cn";
-
-export default function Views({ views }: { views: number }) {
-  return (
-    <div className={cn("absolute left-6 top-6 text-xs text-greyish/80")}>
-      {views.toLocaleString() ?? "?"} views
-    </div>
-  );
-}
