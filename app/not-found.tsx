a8400258f5729e4797f929c0a2ea0ad1ea2eebae
JayanthGummitha <jayanthgummitha@gmail.com>
2024-10-21 17:03:18 -0500

    empty

--- app/not-found.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/cn";
-
-export default function NotFound() {
-  return (
-    <div className={cn("flex items-center justify-center")}>
-      <h1 className={cn("text-4xl font-extrabold")}>404</h1>
-    </div>
-  );
-}
--- components/Header/Logo.tsx
+++
@@ -1,5 +0,0 @@
-import Image from "next/image";
-
-export default function Logo() {
-  return <Image height={34} width={34} src="/next.svg" alt="logo" priority />;
-}
